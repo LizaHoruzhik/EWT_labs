5fcb0cfc65cf3478771d9f7c0d0cd5bf7232993d
LizaHoruzhik <liza.horuzhik@gmail.com>
2025-02-21 13:26:55 +0300

    Update laba 7

--- 7/dop.js
+++ 7/dop.js
@@ -1,11 +1,22 @@
-function setFlexDirection(direction) {
+function setFlexDirection(direction, button) {
     document.querySelector('.container1').style.flexDirection = direction;
+    setActiveButton(button);
 }
 
-function setJustifyContent(justify) {
+function setJustifyContent(justify, button) {
     document.querySelector('.container2').style.justifyContent = justify;
+    setActiveButton(button);
 }
 
-function setAlignItems(align) {
+function setAlignItems(align, button) {
     document.querySelector('.container3').style.alignItems = align;
+    setActiveButton(button);
+}
+
+function setActiveButton(activeButton) {
+    const buttons = activeButton.parentNode.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+    activeButton.classList.add('active');
 }
