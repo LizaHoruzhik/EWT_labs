47524e76e08f5871eab51ab4ea5640a80ccd7dc5
LizaHoruzhik <liza.horuzhik@gmail.com>
2025-02-21 13:15:43 +0300

    Add laba 3

---
+++ 7/dop.js
@@ -0,0 +1,11 @@
+function setFlexDirection(direction) {
+    document.querySelector('.container1').style.flexDirection = direction;
+}
+
+function setJustifyContent(justify) {
+    document.querySelector('.container2').style.justifyContent = justify;
+}
+
+function setAlignItems(align) {
+    document.querySelector('.container3').style.alignItems = align;
+}
